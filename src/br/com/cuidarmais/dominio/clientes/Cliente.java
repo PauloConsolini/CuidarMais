6ecb0c42279c7cbd1600f7c8a16776a392c7e3ea
PauloConsolini <159178924+PauloConsolini@users.noreply.github.com>
2025-08-25 22:30:39 -0300

    Update Cliente.java

--- src/br/com/cuidarmais/dominio/clientes/Cliente.java
+++ src/br/com/cuidarmais/dominio/clientes/Cliente.java
@@ -1,3 +1,75 @@
+package br.com.cuidarmais.dominio.clientes;
+import br.com.cuidarmais.dominio.canais.Canal;
+import br.com.cuidarmais.dominio.sustentabilidade.PerfilSustentavel;
+
+
 public class Cliente {
+    
+    private String nome;
+    private String documento;
+    private int idade;
+    private TipoCliente tipo;
+    private Canal canalPreferido;
+    private PerfilSustentavel perfilSustentavel;
+
+    public Cliente(String nome, String documento, int idade, TipoCliente tipo, Canal canalPreferido, PerfilSustentavel perfilSustentavel) {
+        this.nome = nome;
+        this.documento = documento;
+        this.idade = idade;
+        this.tipo = tipo;
+        this.canalPreferido = canalPreferido;
+        this.perfilSustentavel = perfilSustentavel;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public TipoCliente getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoCliente tipo) {
+        this.tipo = tipo;
+    }
+
+    public Canal getCanalPreferido() {
+        return canalPreferido;
+    }
+
+    public void setCanalPreferido(Canal canalPreferido) {
+        this.canalPreferido = canalPreferido;
+    }
+
+    public PerfilSustentavel getPerfilSustentavel() {
+        return perfilSustentavel;
+    }
+
+    public void setPerfilSustentavel(PerfilSustentavel perfilSustentavel) 
+        this.perfilSustentavel = perfilSustentavel;
+    }
+
 
 }
+
+
