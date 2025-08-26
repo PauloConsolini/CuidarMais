3cc1a7c1a4574455710c2405161eb686ac5df750
PauloConsolini <159178924+PauloConsolini@users.noreply.github.com>
2025-08-25 22:48:18 -0300

    Update SeguroVida.java
    Acrescentando o código fonte referente a classe.

--- src/br/com/cuidarmais/dominio/seguros/SeguroVida.java
+++ src/br/com/cuidarmais/dominio/seguros/SeguroVida.java
@@ -1,5 +1,48 @@
-package br.com.cuidarmais.dominio.clientes;
+package br.com.cuidarmais.dominio.seguros;
+import br.com.cuidarmais.dominio.clientes.Cliente;
 
-public class SeguroVida {
+public class SeguroVida extends SeguroBase {
 
+    public SeguroVida(Cliente cliente, double valorBase) {
+        super(cliente, valorBase);
+    }
+
+    @Override
+    public void avaliarRisco() {
+        int idade = cliente.getIdade();
+
+        if (idade < 30) {
+            this.riscoCalculado = 200.0;
+        } else if (idade <= 49) {
+            this.riscoCalculado = 350.0;
+        } else {
+            this.riscoCalculado = 600.0;
+        }
+    }
+
+    @Override
+    public void calcularPremio() {
+        double valorCoberturas = coberturas.stream()
+            .mapToDouble(Cobertura::getValor)
+            .sum();
+
+        double ajusteSustentavel = calcularAjustePerfilSustentavel();
+
+        this.premioFinal = this.valorBase + this.riscoCalculado + valorCoberturas - ajusteSustentavel;
+
+        System.out.println("Prêmio do Seguro Vida: R$ " + this.premioFinal);
+    }
+
+    private double calcularAjustePerfilSustentavel() {
+        if (cliente.getPerfilSustentavel() == null) return 0.0;
+
+        double indicador = cliente.getPerfilSustentavel().getCarbonoEstimado()
+                + cliente.getPerfilSustentavel().getConsumoEnergiaEstimado()
+                + cliente.getPerfilSustentavel().getConsumoDeAgua();
+
+        if (indicador < 150) return 50.0;
+        else if (indicador < 300) return 25.0;
+        else return 0.0;
+    }
 }
+
