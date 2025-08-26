2a9b92b5ab5364476e26abfdd937acc8938745cd
PauloConsolini <159178924+PauloConsolini@users.noreply.github.com>
2025-08-25 22:43:34 -0300

    Update WhatsappComunicacao.java
    Acrescentando o código fonte referente a classe.

--- src/br/com/cuidarmais/dominio/comunicacao/WhatsappComunicacao.java
+++ src/br/com/cuidarmais/dominio/comunicacao/WhatsappComunicacao.java
@@ -1,5 +1,12 @@
 package br.com.cuidarmais.dominio.comunicacao;
+import br.com.cuidarmais.dominio.clientes.Cliente;
 
-public class WhatsappComunicacao {
-
+public class WhatsappComunicacao implements CanalComunicacao {
+   
+    @Override
+    public void enviarMensagem(Cliente cliente, String mensagem) {
+        System.out.println("[WHATSAPP] Para " + cliente.getNome() + ": " + mensagem);
+    
+    }
+    
 }
