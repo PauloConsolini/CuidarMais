588c09f02cdbbc1607151814e44979f7fd358929
PauloConsolini <159178924+PauloConsolini@users.noreply.github.com>
2025-08-25 22:40:25 -0300

    Update AppComunicacao.java
    Acrescentando o código fonte referente a classe.

--- src/br/com/cuidarmais/dominio/comunicacao/AppComunicacao.java
+++ src/br/com/cuidarmais/dominio/comunicacao/AppComunicacao.java
@@ -1,5 +1,13 @@
 package br.com.cuidarmais.dominio.comunicacao;
+import br.com.cuidarmais.dominio.clientes.Cliente;
 
-public class AppComunicacao {
+public class AppComunicacao implements CanalComunicacao {
+    
+    @Override
+    public void enviarMensagem(Cliente cliente, String mensagem) {
+        System.out.println("[APP] Para " + cliente.getNome() + ": " + mensagem);
+    
+    }
 
 }
+
