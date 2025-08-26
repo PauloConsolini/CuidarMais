628770d73c5233c51631f062c78bda8d550d3501
PauloConsolini <159178924+PauloConsolini@users.noreply.github.com>
2025-08-25 22:41:21 -0300

    Update CanalComunicacao.java
    Acrescentando o código fonte referente a classe.

--- src/br/com/cuidarmais/dominio/comunicacao/CanalComunicacao.java
+++ src/br/com/cuidarmais/dominio/comunicacao/CanalComunicacao.java
@@ -1,5 +1,10 @@
-package br.com.cuidarmais.dominio.seguros;
+package br.com.cuidarmais.dominio.comunicacao;
+import br.com.cuidarmais.dominio.clientes.Cliente;
 
 public interface CanalComunicacao {
 
+        void enviarMensagem(Cliente cliente, String mensagem);
+
 }
+
+
