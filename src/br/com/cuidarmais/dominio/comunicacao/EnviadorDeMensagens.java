129cdb3aa172ef0b86e35089b1d932e8d6e026c3
PauloConsolini <159178924+PauloConsolini@users.noreply.github.com>
2025-08-25 22:42:46 -0300

    Update EnviadorDeMensagens.java
    Acrescentando o código fonte referente a classe.

--- src/br/com/cuidarmais/dominio/comunicacao/EnviadorDeMensagens.java
+++ src/br/com/cuidarmais/dominio/comunicacao/EnviadorDeMensagens.java
@@ -1,5 +1,24 @@
 package br.com.cuidarmais.dominio.comunicacao;
+import br.com.cuidarmais.dominio.clientes.Cliente;
+import br.com.cuidarmais.dominio.canais.Canal;
 
 public class EnviadorDeMensagens {
 
+    public static void notificarCliente(Cliente cliente, String mensagem) {
+        Canal canalPreferido = cliente.getCanalPreferido();
+        CanalComunicacao canalComunicacao;
+
+        switch (canalPreferido) {
+            case APP -> canalComunicacao = new AppComunicacao();
+            case EMAIL -> canalComunicacao = new EmailComunicacao();
+            case WHATSAPP -> canalComunicacao = new WhatsappComunicacao();
+            default -> {
+                System.out.println("Canal não suportado para " + cliente.getNome());
+                return;
+            }
+        }
+
+        canalComunicacao.enviarMensagem(cliente, mensagem);
+    }
 }
+
