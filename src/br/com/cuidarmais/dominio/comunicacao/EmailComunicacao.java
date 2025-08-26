6d7d720ba4d14c0d37332585b5664eadfc2ff6e2
PauloConsolini <159178924+PauloConsolini@users.noreply.github.com>
2025-08-25 22:42:08 -0300

    Update EmailComunicacao.java
    Acrescentando o código fonte referente a classe.

--- src/br/com/cuidarmais/dominio/comunicacao/EmailComunicacao.java
+++ src/br/com/cuidarmais/dominio/comunicacao/EmailComunicacao.java
@@ -1,5 +1,14 @@
 package br.com.cuidarmais.dominio.comunicacao;
+import br.com.cuidarmais.dominio.clientes.Cliente;
 
-public class EmailComunicacao {
-
+public class EmailComunicacao implements CanalComunicacao {
+    
+    @Override
+    public void enviarMensagem(Cliente cliente, String mensagem) {
+        System.out.println("[EMAIL] Para " + cliente.getNome() + ": " + mensagem);
+    
+    }
+    
 }
+
+
