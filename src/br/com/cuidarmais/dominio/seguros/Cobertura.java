39ffcca09373ab033a9ed9e0a7932ce2d2ea0786
PauloConsolini <159178924+PauloConsolini@users.noreply.github.com>
2025-08-25 22:44:34 -0300

    Update Cobertura.java
    Acrescentando o código fonte referente a classe.

--- src/br/com/cuidarmais/dominio/seguros/Cobertura.java
+++ src/br/com/cuidarmais/dominio/seguros/Cobertura.java
@@ -1,5 +1,35 @@
-package br.com.cuidarmais.dominio.clientes;
+package br.com.cuidarmais.dominio.seguros;
 
 public class Cobertura {
 
+    private String nome;
+    private double valor;
+    private NivelRisco nivelRisco;
+
+    public Cobertura(String nome, double valor, NivelRisco nivelRisco) {
+        this.nome = nome;
+        this.valor = valor;
+        this.nivelRisco = nivelRisco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public NivelRisco getNivelRisco() {
+        return nivelRisco;
+    }
+
+    @Override
+    public String toString() {
+        return "Cobertura{" +
+                "nome='" + nome + '\'' +
+                ", valor=" + valor +
+                ", nivelRisco=" + nivelRisco +
+                '}';
+    }
 }
