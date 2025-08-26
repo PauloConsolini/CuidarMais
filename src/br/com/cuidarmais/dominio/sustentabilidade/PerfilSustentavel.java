0e2c19a68d6a87490372d3db2a2003033de71f46
PauloConsolini <159178924+PauloConsolini@users.noreply.github.com>
2025-08-25 22:48:57 -0300

    Update PerfilSustentavel.java
    Acrescentando o código fonte referente a classe.

--- src/br/com/cuidarmais/dominio/sustentabilidade/PerfilSustentavel.java
+++ src/br/com/cuidarmais/dominio/sustentabilidade/PerfilSustentavel.java
@@ -1,3 +1,40 @@
+package br.com.cuidarmais.dominio.sustentabilidade;
+
 public class PerfilSustentavel {
 
+    private double consumoEnergiaEstimado;
+    private double carbonoEstimado;
+    private double consumoDeAgua;
+
+    public PerfilSustentavel(double consumoEnergiaEstimado, double carbonoEstimado, double consumoDeAgua) {
+       
+        this.consumoEnergiaEstimado = consumoEnergiaEstimado;
+        this.carbonoEstimado = carbonoEstimado;
+        this.consumoDeAgua = consumoDeAgua;
+    }
+
+    public double getConsumoEnergiaEstimado() {
+        return consumoEnergiaEstimado;
+    }
+
+    public void setConsumoEnergiaEstimado(double consumoEnergiaEstimado) {
+        this.consumoEnergiaEstimado = consumoEnergiaEstimado;
+    }
+
+    public double getCarbonoEstimado() {
+        return carbonoEstimado;
+    }
+
+    public void setCarbonoEstimado(double carbonoEstimado) {
+        this.carbonoEstimado = carbonoEstimado;
+    }
+
+    public double getConsumoDeAgua() {
+        return consumoDeAgua;
+    }
+
+    public void setConsumoDeAgua(double consumoDeAgua) {
+        this.consumoDeAgua = consumoDeAgua;
+    }
 }
+
